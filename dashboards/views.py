e42c41868b744fd8c6d7961cd27c723b6545a38a
Dmytro Soroka <dimaa@google.com>
2017-01-25 15:13:35 -0500

    Created basic page that can make ajax request

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 def index(request):
   return render(request, 'dashboards/index.html')
+
+def get_data(request):
+  json_obj = json.dumps(request.GET)
+  return HttpResponse(json_obj, content_type='application/json')
 # Create your views here.
